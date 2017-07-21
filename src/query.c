c395fa94af392154ca76e72fe7ba9338ba4fad4d
Mark Nunberg <mnunberg@haskalah.org>
2017-07-21 06:30:55 -0700

    Document_Free: accept pointer rather than by-value
    We should consider making this an inline function or macro

--- src/query.c
+++ src/query.c
@@ -951,7 +951,7 @@ int QueryResult_Serialize(QueryResult *r, RedisSearchCtx *sctx, RSSearchRequest 
       if (rkey) {
         RedisModule_CloseKey(rkey);
       }
-      Document_Free(doc);
+      Document_Free(&doc);
     }
   }
 
